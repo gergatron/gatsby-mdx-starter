9ce31331505eae4fbbc6f85827b00f84312c326d
gerg <gregross.dev@gmail.com>
2021-03-05 23:38:49 -0500

    re-init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
-import {graphql} from "gatsby"
 
-const Home = ({data}) =>  {
+const Home = () =>  {
   return (
    <div>Hello world!</div>
   )
@@ -9,25 +8,3 @@ const Home = ({data}) =>  {
 
 
 export default Home
-
-
- // Get all markdown data, in descending order by date, and grab the id, excerpt, slug, date, and title
- export const pageQuery = graphql`
-  query {
-    allMdx(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          id
-          excerpt(pruneLength: 250)
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-          }
-        }
-      }
-    }
-  }
-`;
