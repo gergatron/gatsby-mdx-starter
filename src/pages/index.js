6713f820862ad1b525913f8e3814a76d91861614
gerg <gregross.dev@gmail.com>
2021-03-06 01:13:49 -0500

    post loop

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,41 @@
 import React from "react"
+import { graphql, Link } from 'gatsby';
 
-const Home = () =>  {
+const Home = ({ data }) =>  {
+  const posts = data.allMdx.edges;
   return (
-   <div>Hello world!</div>
+  <div>
+    {posts.map(post => (
+      <div key={post.node.id}>
+        <h2>{post.node.frontmatter.title}</h2>
+        <p>{post.node.frontmatter.date}</p>
+        <p>{post.node.frontmatter.excerpt}</p>
+        <Link to={post.node.fields.slug}>Read More</Link>
+      </div>
+    ))}
+  </div>
   )
 }
 
 export default Home
+
+// Get all markdown data, in descending order by date, and grab the id, excerpt, slug, date, and title
+export const pageQuery = graphql`
+  query {
+    allMdx(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`;
