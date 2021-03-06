ec6231d203bb7b92f1dcd3b55344d579b478d210
gerg <gregross.dev@gmail.com>
2021-03-06 00:07:05 -0500

    createPages

---
+++ gatsby-node.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+
+  const postTemplate = path.resolve(`src/templates/postTemplate.js`);
+
+  return graphql(`
+    {
+      allMdx(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors);
+    }
+
+    result.data.allMdx.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: postTemplate,
+        context: {}, // additional data can be passed via context
+      });
+    });
+  });
+};
